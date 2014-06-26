24f025b7d2f80d85ab9c41387419f84ece9fd118
Joel Mukuthu <joelmukuthu@gmail.com>
2014-06-26 11:06:27 +0300

    use flash for alerts and notices

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,18 +4,25 @@ require 'sp_api'
 class OffersController < ApplicationController
   def index
     @offers = []
-    if params[:commit]
+    if request.post?
       if params[:uid].empty? || params[:pub0].empty? || params[:page].empty?
-        @fields_unfilled = true
+        flash[:alert] = "Please fill all the form fields"
       else
         begin
+          # TODO: use a dropdown for page param? can be updated according to the 'pages' param returned by api..
           page = params[:page].to_i
           page = page < 1 ? 1 : page
           @offers = SpApi.fetch_offers params[:uid], params[:pub0], page
         rescue Exception => e
-          @api_error = e.message
+          if Rails.env.production?
+            flash[:alert] = "Sorry, an API error occurred"
+          else
+            flash[:alert] = "An API error occurred: #{e.message}"
+          end
         end
       end
+    else
+      flash.clear
     end
   end
 end
